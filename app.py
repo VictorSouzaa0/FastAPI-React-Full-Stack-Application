07a1c82201f6c665c237a6d8cbca8313530ae4cf
VictorSouzaa0 <victor.r.souza05@gmail.com>
2025-04-23 08:10:57 -0300

    models

--- app.py
+++ app.py
@@ -1,12 +1,61 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
+from models import student_Pydantic,student_PydanticIn,Student
+from models import instructor_Pydantic,instructor_PydanticIn,Instructor
+from models import class_Pydantic,class_PydanticIn,Classroom
 
 app = FastAPI()
+@app.get('/')
+def index():
+    return{"Msg":"go to /docs for the API documentation"}
+
+
+@app.post('/class')
+async def add_class(class_info: class_PydanticIn):
+    class_obj = await Classroom.create(**class_info.dict(exclude_unset=True))
+    response = await class_Pydantic.from_tortoise_orm(class_obj)
+    return {"status":"ok","data" :response}
+
+@app.get('/class')
+async def get_all_class():
+    response = await class_Pydantic.from_queryset(Classroom.all())
+    return{"Status": "ok","data": response}
+
+@app.get('/class/{class_id}')
+async def get_specific_class(class_id: int):
+    response = await class_Pydantic.from_queryset_single(Classroom.get(id=class_id))
+    return {"Status":"ok","data": response}
+
+@app.put('/class/{class_id}')
+async def update_class(class_id:int , update_info: class_PydanticIn):
+    classs = await Classroom.get(id=class_id)
+    update_info = update_info.dict(exclude_unset=True)
+    classs.name = update_info['name']
+    await classs.save()
+    response =  await class_Pydantic.from_tortoise_orm(classs)
+    return {"Status":"ok","data": response}
+
+@app.delete('/class/{class_id}')
+async def delete_class(class_id: int):
+    response = await Classroom.get(id=class_id).delete()
+    return{"Status":"ok","data":response}
+
+async def add_instructor(instructor_info: instructor_PydanticIn):
+    instructor_obj = await Instructor.create(**instructor_info.dict(exclude_unset=True))
+    response = await instructor_Pydantic.from_tortoise_orm(instructor_obj)
+    return {"status":"ok","data" :response}
+
+@app.get('/instructor')
+async def get_all_instructors():
+    response = await instructor_Pydantic.from_queryset(Instructor.all())
+    return {"status": "ok", "data": response}
+
+
 
 register_tortoise(
     app,
     db_url="sqlite://database.sqlite3",
     modules={"models": ["models"]},
-    generate_schemas=True,
-    add_exception_handlers=True
+    generate_schemas=True,  
+    add_exception_handlers=True,
 )
--- models.py
+++ models.py
@@ -1,25 +1,40 @@
 from tortoise.models import Model
 from tortoise import fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 
-class Class(Model):
+class Classroom(Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=100,null=False)
 
 class Instructor(Model):
     id = fields.IntField(pk=True)
-    full_name = fields.CharField(max_length=255, null=False)
+    fullname = fields.CharField(max_length=255, null=False)
     age = fields.IntField(null=False)
-    
-    class_group = fields.ForeignKeyField('models.Class',
+    classgroup = fields.ForeignKeyField('models.Classroom',
     related_name='instructor')
 
-class Students(Model):
+class Student(Model):
     id = fields.IntField(pk=True)
-    full_name = fields.CharField(max_length=255, null=False)
+    fullname = fields.CharField(max_length=255, null=False)
     age = fields.IntField(null=False)
     carrer_interest = fields.CharField(max_length=255)
 
     instructor = fields.ForeignKeyField('models.Instructor',
     related_name='students')
 
-    
+
+#pydantic_model_creator -  É usado para converter automaticamente os dados tortoise ORM para JSON 
+
+
+#Student
+student_Pydantic = pydantic_model_creator(Student, name="Student")
+student_PydanticIn = pydantic_model_creator(Student,name="StudentIn",exclude_readonly=True)
+
+#Instructor
+instructor_Pydantic = pydantic_model_creator(Instructor, name="Instructor")
+instructor_PydanticIn = pydantic_model_creator(Instructor, name="InstructorIn", exclude_readonly=True)
+
+
+#Class
+class_Pydantic = pydantic_model_creator(Classroom, name="Class")
+class_PydanticIn = pydantic_model_creator(Classroom, name="ClassIn", exclude_readonly=True)
